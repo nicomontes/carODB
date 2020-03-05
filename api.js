c82f7d835a3dac098123c85bd19d67486d3da9b0
nmontes <nmontes@max174-nmo.unil.ch>
2020-03-05 16:10:08 +0100

    update

--- api.js
+++ api.js
@@ -109,13 +109,14 @@ io.sockets.on('connection', function (socket){
 
 				for (var i=0;i<coll.length;i++)
 				{
+					collId = coll[i].name.match(/[0-9]{13}/g)
 					if (coll[i].name.match(/[0-9]{13}/g) != null) {
-						var collection = db.collection(coll[i].name);
+						var collection = db.collection(collId);
 						collection.findOne({}, {email:1}, function(err, item) {
 							if (item.email == email) {
-								var timestamp = item.time.match(/^[0-9]{10}/g)[0]*1000;
+								var timestamp = collId.match(/^[0-9]{10}/g)[0]*1000;
 								var date = new Date(timestamp);
-								selectObject[item.time.match(/[0-9]{13}/g)]=date.toLocaleString();
+								selectObject[collId]=date.toLocaleString();
 								socket.emit('date', selectObject);
 								selectObject = {}
 							}
