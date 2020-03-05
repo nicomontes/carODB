768614b46330aae6fb9452245c7f15641ed69741
nmontes <nmontes@max174-nmo.unil.ch>
2020-03-05 15:39:27 +0100

    update

--- map.js
+++ map.js
@@ -434,8 +434,6 @@ function sendEmail() {
   socket.emit('searchMongo', $('#email')[0].value)
   socket.on('date', function (data){
     var select = document.getElementById("select");
-    var date = new Date(index.match(/^[0-9]{10}/g)[0]*1000);
-    select.options[select.options.length] = new Option(date.toLocaleString(), index);
     for(index in data) {
       var date = new Date(index.match(/^[0-9]{10}/g)[0]*1000);
       select.options[select.options.length] = new Option(date.toLocaleString(), index);
